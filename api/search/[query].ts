139be32d7e13a973f0737ed8d7e3f56013abee15
Near111111 <Jomarmerej@gmail.com>
2026-01-14 16:29:50 +0800

    Add serverless functions with correct structure

--- api/search/[query].ts
+++ api/search/[query].ts
@@ -14,7 +14,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const data = await response.json();
 
-    // Enable CORS
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
 
